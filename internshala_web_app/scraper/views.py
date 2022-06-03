b9375c593900920daab853b34f84c781827099ee
inAsees <gurasees.singh121@gmail.com>
2022-06-04 01:41:02 +0530

    1. inside index.html , href values modified and fixed for bug 2. about.html added to templates. 3. in scraper.urls path added. 4. in scraper.views  view about added

--- internshala_web_app/scraper/views.py
+++ internshala_web_app/scraper/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, './index.html')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+

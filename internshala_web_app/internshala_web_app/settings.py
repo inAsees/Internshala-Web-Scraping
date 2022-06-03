9d4c79bd85b535f373473d1a0b704b1807ebc794
inAsees <gurasees.singh121@gmail.com>
2022-06-03 13:04:11 +0530

    database config fixed for bug.

--- internshala_web_app/internshala_web_app/settings.py
+++ internshala_web_app/internshala_web_app/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-9nlmnweocvg%4c)pso9i%i0d3d@oq0fs2mlm)u0iaeev+82fx^
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,20 +68,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'internshala_web_app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'NAME': 'my_db',
+    'default': {
+        'NAME': 'my_db',
         'ENGINE': 'mysql.connector.django',
         'USER': 'root',
         'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
         'OPTIONS': {
-          'autocommit': True,
+            'autocommit': True,
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -103,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -114,7 +113,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/

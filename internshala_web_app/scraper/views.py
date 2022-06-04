1ee8cf1fc68e4a3fb88859ab612a812acfdec5d1
inAsees <gurasees.singh121@gmail.com>
2022-06-04 18:59:29 +0530

    1. in services-scraping_website.html , name attribute added to select tag. 2. view scraper updated.

--- internshala_web_app/scraper/views.py
+++ internshala_web_app/scraper/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from backend.src.scrapper import ScrapInternshala
 from backend.src.utils import Utils
 
 
@@ -17,8 +18,14 @@ def services(request):
 
 
 def scraper(request):
-    keywords = Utils.get_available_keywords()
-    return render(request, 'services-scraping_website.html', {'keywords': keywords})
+    if request.method == "POST":
+        keyword_search = request.POST['keyword']
+        scrap_internshala = ScrapInternshala(keyword_search)
+        scrap_internshala.scrap_all_pages()
+        return render(request, 'services-scraper_website.html')
+    else:
+        keywords = Utils.get_available_keywords()
+        return render(request, 'services-scraping_website.html', {'keywords': keywords})
 
 
 def analytics(request):

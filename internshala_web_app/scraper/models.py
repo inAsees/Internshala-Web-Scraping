ec2b62f3f16f8adeaafab669f8ed873315530c23
inAsees <gurasees.singh121@gmail.com>
2022-06-04 15:53:18 +0530

    class InternshipDetail created in scraper.models
    1. superuser created and model registered with the admin.

--- internshala_web_app/scraper/models.py
+++ internshala_web_app/scraper/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class InternshipDetail(models.Model):
+    internship_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=200)
+    monthly_stipend = models.IntegerField()
+    weekly_stipend = models.IntegerField()
+    incentives = models.CharField(max_length=200)
+    duration = models.IntegerField()
+    location = models.CharField(max_length=100)
+    last_date_to_apply = models.DateField()
+    applicants_count = models.IntegerField()
+    number_of_openings = models.IntegerField()
+    skills = models.CharField(max_length=300)
+    perks = models.CharField(max_length=300)
+    src_url = models.CharField(max_length=300)
+    status = models.CharField(max_length=50)

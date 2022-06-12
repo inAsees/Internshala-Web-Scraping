b213e7e6744b39901e507e46af0bfd8bd2d3848e
inAsees <gurasees.singh121@gmail.com>
2022-06-12 17:12:49 +0530

    1. .env added in .gitignore 2. secret key settings updated in  settings.py

--- internshala_web_app/internshala_web_app/settings.py
+++ internshala_web_app/internshala_web_app/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9nlmnweocvg%4c)pso9i%i0d3d@oq0fs2mlm)u0iaeev+82fx^'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.environ.get('Secret_Key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
